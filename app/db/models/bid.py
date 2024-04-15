c5cfc2dab6746f329e68e6d8c5ac6fa0175f8749
jusruizsa <sruizs.jr@gmail.com>
2024-04-14 21:08:12 -0500

    add bid class doc

--- app/db/models/bid.py
+++ app/db/models/bid.py
@@ -2,6 +2,19 @@ from abc import ABC, abstractmethod
 
 
 class Bid():
+    """
+    Represents a bid in an auction.
+
+    Attributes:
+        id (int): The unique identifier of the bid.
+        auction_id (int): The ID of the auction the bid belongs to.
+        user_id (int): The ID of the user who placed the bid.
+        amount (float): The amount of the bid.
+        duration (int): The duration of the bid in minutes.
+        state (str): The state of the bid (e.g., "active", "finished", "canceled").
+        created_at (datetime): The timestamp when the bid was created.
+    """
+
     def __init__(self, id, auction_id, user_id, amount, duration, state, created_at):
         self.id = id
         self.auction_id = auction_id
