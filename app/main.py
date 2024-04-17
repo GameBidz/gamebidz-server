028a31ca1dbc0e35a994812cea7311d3a16e9054
jusruizsa <sruizs.jr@gmail.com>
2024-04-16 23:49:26 -0500

    add auction router in the app

--- app/main.py
+++ app/main.py
@@ -1,6 +1,10 @@
 from fastapi import FastAPI
 from app.routes.user_routes import router as user_router
+from app.routes.auction_routes import router as auction_router
 
 app = FastAPI()
 
+# Include the user router in the app.
 app.include_router(user_router)
+# Include the auction router in the app.
+app.include_router(auction_router)
