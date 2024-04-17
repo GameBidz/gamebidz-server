80d76a2744acca796ed71950830393f6eb3354d9
jusruizsa <sruizs.jr@gmail.com>
2024-04-16 23:51:55 -0500

    add routes for auctions

---
+++ app/routes/auction_routes.py
@@ -0,0 +1,56 @@
+from fastapi import APIRouter, Response, status
+from app.controllers.auction_controller import AuctionController
+from app.services.auction_service import AuctionDefault
+from app.repositories.auction_repository import AuctionMySQL
+from app.db.db import connection
+
+
+# Create a new instance of the AuctionMySQL class.
+auction_repository = AuctionMySQL(connection)
+# Create a new instance of the AuctionDefault class.
+auction_service = AuctionDefault(auction_repository)
+# Create a new instance of the AuctionController class.
+auction_controller = AuctionController(auction_service)
+
+# Define the router for the auction routes.
+router = APIRouter()
+
+
+# Define the route for retrieving all auctions.
+@router.get("/auctions")
+def get_auctions():
+    return {"auctions": auction_controller.get_auctions()}
+
+
+# Define the route for retrieving an auction by its ID.
+@router.get("/auctions/{auction_id}")
+def get_auction_by_id(auction_id: int, response: Response):
+    auction = auction_controller.get_auction_by_id(auction_id)
+    if not auction:
+        response.status_code = status.HTTP_404_NOT_FOUND
+        return {"error": "Auction not found"}
+    return {"auction": auction}
+
+
+# Define the route for retrieving all auctions for a user.
+@router.get("/auctions/user/{user_id}")
+def get_auctions_by_user_id(user_id: int):
+    return {"auctions": auction_controller.get_auctions_by_user_id(user_id)}
+
+
+# Define the route for creating a new auction.
+@router.post("/auctions")
+def create_auction(auction):
+    return {"auction": auction_controller.create_auction(auction)}
+
+
+# Define the route for updating an auction.
+@router.put("/auctions")
+def update_auction(auction):
+    return {"auction": auction_controller.update_auction(auction)}
+
+
+# Define the route for deleting an auction.
+@router.delete("/auctions/{auction_id}")
+def delete_auction(auction_id: int):
+    return {"auction": auction_controller.delete_auction(auction_id)}
