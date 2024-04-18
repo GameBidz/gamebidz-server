f0caba12f8d057093c2164109e0f605db7ff4114
jusruizsa <sruizs.jr@gmail.com>
2024-04-17 22:35:59 -0500

    add create_auction and update_function so the service conforms to the abstract AuctionService class

--- app/services/auction_service.py
+++ app/services/auction_service.py
@@ -41,3 +41,29 @@ class AuctionDefault(AuctionService):
             List[Auction]: A list of auction objects.
         """
         return self.auction_repository.get_auctions_by_user_id(user_id)
+
+    def create_auction(self, user_id, initial_amount, duration, state, created_at):
+        """Create a new auction.
+
+        Args:
+            user_id (int): The ID of the user.
+            initial_amount (float): The initial amount of the auction.
+            duration (int): The duration of the auction.
+            state (str): The state of the auction.
+            created_at (datetime): The creation date of the auction.
+
+        Returns:
+            Auction: The auction object.
+        """
+        return self.auction_repository.create_auction(user_id, initial_amount, duration, state, created_at)
+
+    def update_auction(self, auction):
+        """Update an auction.
+
+        Args:
+            auction (Auction): The auction object.
+
+        Returns:
+            Auction: The updated auction object.
+        """
+        return self.auction_repository.update_auction(auction)
