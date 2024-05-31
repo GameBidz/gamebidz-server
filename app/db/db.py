c8112f5dbbbb1872660776d8a68e9242eb240248
jusruizsa <sruizs.jr@gmail.com>
2024-05-31 14:22:04 -0500

    update db config

--- app/db/db.py
+++ app/db/db.py
@@ -3,12 +3,13 @@ import os
 import dotenv
 
 # Load environment variables from the .env file
-dotenv.load_dotenv('app/.env')
+dotenv.load_dotenv('.env')
 
 # Establish a connection to the database
 connection = pymysql.connect(
-    host=os.environ.get("DB_HOST"),
-    user=os.environ.get("DB_USER"),
-    password=os.environ.get("DB_PASS"),
-    database=os.environ.get("DB_NAME"),
+    host="localhost",
+    user=os.environ.get("DATABASE_USER"),
+    password=os.environ.get("DATABASE_PASS"),
+    database=os.environ.get("DATABASE_NAME"),
+    port=int(os.environ.get("DATABASE_PORT")),
 )
